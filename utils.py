16e1f085917876a3f8ce4a66cdb649c6f4df61c2
Kacper <50482739+Kacper777777@users.noreply.github.com>
2021-05-25 15:51:49 +0200

    Fix the path to the project in utils.py.

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import os
 
-PROJECT_REAL_PATH = os.path.dirname(os.path.realpath(__file__))[:-6]
+PROJECT_REAL_PATH = os.path.dirname(os.path.realpath(__file__))
 
 DATA_REAL_PATH = PROJECT_REAL_PATH + '\\gitignore'
