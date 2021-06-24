dddbc888aee8b4f87dcde2e588078f475b21de4f
Kacper <50482739+Kacper777777@users.noreply.github.com>
2021-06-24 16:13:01 +0200

    Add custom generator class. Small changes in CenterNetDetector class.

--- centernet_detector.py
+++ centernet_detector.py
@@ -1,45 +1,31 @@
-import cv2
 import numpy as np
-from data_preprocessing.padding_and_cutting import resize_and_pad
 from models_factory import ModelsFactory
 
 
 class CenterNetDetector:
     def __init__(self, model_name, input_shape, classes_list, max_objects,
-                 resize_and_pad, grayscale, scale_values):
+                 image_preprocessor=None):
         self.input_shape = input_shape
         self.classes_list = classes_list
         self.num_classes = len(classes_list)
         self.max_objects = max_objects
-        self.models_factory = ModelsFactory()
-        self.model, self.prediction_model, self.debug_model = self.set_models(model_name)
-
-        # preprocessing options
-        self.resize_and_pad = resize_and_pad
-        self.rgb_weights = [0.2989, 0.5870, 0.1140] if grayscale else [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-        self.channels = 1 if grayscale else 3
-        self.scale_values = scale_values
+        self.model, self.prediction_model, self.debug_model, self.downsample_factor\
+            = self.set_models(model_name)
+        self.image_preprocessor = image_preprocessor
 
     def set_models(self, model_name):
-        return self.models_factory.get_model(model_name, self.input_shape, self.num_classes, self.max_objects)
+        return ModelsFactory.get_model(model_name, self.input_shape, self.num_classes, self.max_objects)
 
     def load_weights(self, model_path):
-        self.prediction_model.load_weights(model_path, by_name=True, skip_mismatch=True)
-        self.debug_model.load_weights(model_path, by_name=True, skip_mismatch=True)
+        self.prediction_model.load_weights(model_path, by_name=False, skip_mismatch=False)
+        self.debug_model.load_weights(model_path, by_name=False, skip_mismatch=False)
 
-    def preprocess_inputs(self, inputs):
-        if self.resize_and_pad:
-            for i in range(len(inputs)):
-                inputs[i], _ = resize_and_pad(inputs[i], (self.input_shape[0], self.input_shape[1]))
-        inputs = np.dot(inputs[..., :3], self.rgb_weights)
-        inputs = inputs.reshape((inputs.shape[0], inputs.shape[1], inputs.shape[2], self.channels))
-        return inputs * self.scale_values
-
-    def detect(self, inputs, score_threshold):
-        inputs = self.preprocess_inputs(inputs)
-        detections = self.prediction_model.predict_on_batch(inputs)[0]
+    def detect(self, image, score_threshold):
+        image = self.image_preprocessor.preprocess_image(image)
+        image = np.expand_dims(image, axis=0)
+        detections = self.prediction_model.predict_on_batch(image)[0]
         detections = np.array(detections)
-        detections[:, 0:4] *= 4
+        detections[:, 0:4] *= self.downsample_factor
         scores = detections[:, 4]
         # find indices which have a score above the threshold
         indices = np.where(scores > score_threshold)[0]
---
+++ data_preprocessing/generator.py
@@ -0,0 +1,100 @@
+import cv2
+import math
+import numpy as np
+import random
+from data_preprocessing.gaussian import gaussian_radius, draw_gaussian
+from data_preprocessing.image_preprocessor import ImagePreprocessor
+from tensorflow_core.python.keras.utils.data_utils import Sequence
+
+
+class CustomGenerator(Sequence):
+    def __init__(self, preprocessing_strategy, input_size, grayscale, downsample_factor,
+                 num_classes, max_objects,
+                 image_names, batch_size):
+        self.__preprocessing_strategy = preprocessing_strategy
+        self.__input_size = input_size
+        self.__grayscale = grayscale
+        self.__downsample_factor = downsample_factor
+        self.__output_size = input_size // downsample_factor
+        self.__num_classes = num_classes
+        self.__max_objects = max_objects
+        self.__image_names = image_names
+        self.__batch_size = batch_size
+        self.__batch_number = 0
+        self.__image_preprocessor = ImagePreprocessor(preprocessing_strategy=preprocessing_strategy,
+                                                      target_shape=self.__input_size,
+                                                      grayscale=grayscale)
+
+    def __len__(self):
+        """Denotes the number of batches per epoch
+            :return: number of batches per epoch
+        """
+        return int(np.floor(len(self.__image_names) / self.__batch_size))
+
+    def __getitem__(self, item):
+        """Generate one batch of data"""
+        selected_image_names_indices = range(self.__batch_number * self.__batch_size,
+                                             (self.__batch_number + 1) * self.__batch_size)
+        print('selected_image_names_indices', selected_image_names_indices)
+        X = self._generate_X(selected_image_names_indices)
+        y = self._generate_y(selected_image_names_indices)
+        self.__batch_number += 1
+        if self.__batch_number >= self.__len__():
+            self.__batch_number = 0
+        return X, y
+
+    def on_epoch_end(self):
+        if self.shuffle:
+            random.shuffle(self.__image_names)
+
+    def _generate_X(self, selected_image_names_indices):
+        channels = 1 if self.__grayscale else 3
+        images = np.zeros((len(selected_image_names_indices), self.__input_size, self.__input_size, channels),
+                          dtype=np.float32)
+        hms = np.zeros((len(selected_image_names_indices), self.__output_size, self.__output_size, self.__num_classes),
+                       dtype=np.float32)
+        whs = np.zeros((len(selected_image_names_indices), self.__max_objects, 2), dtype=np.float32)
+        regs = np.zeros((len(selected_image_names_indices), self.__max_objects, 2), dtype=np.float32)
+        reg_masks = np.zeros((len(selected_image_names_indices), self.__max_objects), dtype=np.float32)
+        indices = np.zeros((len(selected_image_names_indices), self.__max_objects), dtype=np.float32)
+
+        file_index = 0
+        selected_image_names = [self.__image_names[index] for index in selected_image_names_indices]
+        for file in selected_image_names:
+            img = cv2.imread(file)
+            img = self.__image_preprocessor.preprocess_image(img)
+            scaled_image_dims = self.__image_preprocessor.scaled_image_dims(img)
+            images[file_index] = img
+
+            with open(f'{file[:-4]}.txt') as reader:
+                lines = reader.readlines()
+                bbox_index = 0
+                for line in lines:
+                    object_class, x_center, y_center, w, h = line.split(' ')
+                    object_class, x_center, y_center, w, h = \
+                        float(object_class), float(x_center), float(y_center), float(w), float(h)
+
+                    x_center, y_center = x_center * scaled_image_dims[1], y_center * scaled_image_dims[0]
+                    w, h = w * scaled_image_dims[1], h * scaled_image_dims[0]
+                    x_center += (self.__input_size - scaled_image_dims[1]) / 2
+                    y_center += (self.__input_size - scaled_image_dims[0]) / 2
+
+                    x_center, y_center = x_center / self.__downsample_factor, y_center / self.__downsample_factor
+                    w, h = w / self.__downsample_factor, h / self.__downsample_factor
+
+                    ct = np.array([x_center, y_center], dtype=np.float32)
+                    ct_int = ct.astype(np.int32)
+                    radius = gaussian_radius((max(math.ceil(h), 1), max(math.ceil(w), 1)), min_overlap=0.3)
+                    draw_gaussian(hms[file_index, :, :, int(object_class)], ct_int, int(radius), radius / 2)
+                    whs[file_index, bbox_index] = 1. * w, 1. * h
+                    regs[file_index, bbox_index] = ct - ct_int
+                    reg_masks[file_index, bbox_index] = 1
+                    indices[file_index, bbox_index] = (ct_int[1] * self.__output_size + ct_int[0])
+                    bbox_index += 1
+
+            file_index += 1
+
+        return [selected_image_names, images, hms, whs, regs, reg_masks, indices]
+
+    def _generate_y(self, selected_image_names_indices):
+        return np.zeros(len(selected_image_names_indices))
