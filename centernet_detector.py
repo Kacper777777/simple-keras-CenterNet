eac75a042810a92c34159bd0454b7279b87812e2
Kacper <50482739+Kacper777777@users.noreply.github.com>
2021-11-20 20:20:54 +0100

    Refactor centernet_detector.py

--- centernet_detector.py
+++ centernet_detector.py
@@ -1,20 +1,18 @@
 import numpy as np
-from models_factory import ModelsFactory
 
 
 class CenterNetDetector:
-    def __init__(self, model_name, input_shape, classes_list, max_objects,
-                 image_preprocessor=None):
+    def __init__(self, model, prediction_model, debug_model, downsample_factor,
+                 input_shape, classes_list, max_objects, image_preprocessor):
+        self.model = model
+        self.prediction_model = prediction_model
+        self.debug_model = debug_model
+        self.downsample_factor = downsample_factor
         self.input_shape = input_shape
         self.classes_list = classes_list
         self.num_classes = len(classes_list)
         self.max_objects = max_objects
-        self.model, self.prediction_model, self.debug_model, self.downsample_factor\
-            = self.set_models(model_name)
         self.image_preprocessor = image_preprocessor
-
-    def set_models(self, model_name):
-        return ModelsFactory.get_model(model_name, self.input_shape, self.num_classes, self.max_objects)
 
     def load_weights(self, model_path):
         self.prediction_model.load_weights(model_path, by_name=False, skip_mismatch=False)
