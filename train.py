9da2e4cfca1d43390acd8119a19488b952ec3e37
Kacper <50482739+Kacper777777@users.noreply.github.com>
2022-01-02 11:53:30 +0100

    Use tf.data.Dataset instead of the keras.utils.Sequence class to handle dataset creation. It is much faster. Refactor train.py.

--- train.py
+++ train.py
@@ -4,21 +4,27 @@ import random
 import os
 import glob
 from utils import DATA_REAL_PATH
-from data_preprocessing.generator import CenterNetGenerator
+from training.prepare_dataset import ObjectDetectionDataset
 from models.squezenet import squeezenet_centernet
+import argparse
 
 
 def main():
-    # for reproducibility
+    # Check if GPU  is enabled
+    print(tf.config.list_physical_devices("GPU"))
+    # For reproducibility
     np.random.seed(42)
     random.seed(42)
-    # for readability
+    # For readability
     np.set_printoptions(formatter={'float': lambda x: "{0:0.2f}".format(x)})
 
-    # configuration
-    model_path = os.path.join(DATA_REAL_PATH, 'NEWEST_MODEL')
-    checkpoint_path = os.path.join(model_path, 'checkpoint_dir', 'cp.ckpt')
-    checkpoint_dir = os.path.dirname(checkpoint_path)
+    # Dataset and training configuration
+    pngs = glob.glob(os.path.join(DATA_REAL_PATH, 'datasets', 'chessboards/*.png'))
+    jpgs = glob.glob(os.path.join(DATA_REAL_PATH, 'datasets', 'chessboards/*.jpg'))
+    image_names = pngs + jpgs
+    shuffle_data = True
+    batch_size = 16
+    preprocessing_strategy = 'resize_with_pad'
     input_size = 256
     channels = 3
     grayscale = False if channels == 3 else True
@@ -26,68 +32,62 @@ def main():
     classes_list = ['chessboard']
     num_classes = len(classes_list)
     max_objects = 1
+    train_ratio = 0.8
+    epochs = 50
 
+    # create train and test sets
+    ds_train, ds_test = ObjectDetectionDataset(image_names=image_names,
+                                               shuffle_data=shuffle_data,
+                                               batch_size=batch_size,
+                                               preprocessing_strategy=preprocessing_strategy,
+                                               input_size=input_size,
+                                               grayscale=grayscale,
+                                               downsample_factor=downsample_factor,
+                                               num_classes=num_classes,
+                                               max_objects=max_objects,
+                                               ).create_datasets(train_ratio=train_ratio)
+
+    print("train size: ", ds_train.cardinality().numpy())
+    print("test size: ", ds_test.cardinality().numpy())
+
+    # Model paths
+    model_path = os.path.join(DATA_REAL_PATH, 'newest_model')
+    checkpoint_path = os.path.join(model_path, 'checkpoint_dir', 'cp.ckpt')
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+
+    # Model creation
     model = squeezenet_centernet(image_shape=(input_size, input_size, channels),
                                  num_classes=num_classes,
                                  max_objects=max_objects).get('train_model')
 
-    #model.load_weights(os.path.join(model_path, 'model_tf_format', 'model'))
-    #model.load_weights(checkpoint_path)
+    # Load weights
+    # model.load_weights(os.path.join(model_path, 'model_tf_format', 'model'))
+    # model.load_weights(checkpoint_path)
 
-    # load image names
-    pngs = glob.glob(os.path.join(DATA_REAL_PATH, 'datasets', 'chessboards/*.png'))
-    jpgs = glob.glob(os.path.join(DATA_REAL_PATH, 'datasets', 'chessboards/*.jpg'))
-    image_names = pngs + jpgs
-    # random.shuffle(image_names)
-
-    # training configuration
-
-    # create custom generators
-    train_gen = CenterNetGenerator(shuffle=True,
-                                   preprocessing_strategy='resize_with_pad',
-                                   input_size=input_size,
-                                   grayscale=grayscale,
-                                   downsample_factor=downsample_factor,
-                                   num_classes=num_classes,
-                                   max_objects=max_objects,
-                                   image_names=image_names[:int(0.8 * len(image_names))],
-                                   batch_size=32)
-
-    val_gen = CenterNetGenerator(shuffle=True,
-                                 preprocessing_strategy='resize_with_pad',
-                                 input_size=input_size,
-                                 grayscale=grayscale,
-                                 downsample_factor=downsample_factor,
-                                 num_classes=num_classes,
-                                 max_objects=max_objects,
-                                 image_names=image_names[int(0.8 * len(image_names)):],
-                                 batch_size=32)
-
-    epochs = 50
+    # Some useful callbacks
 
     early_stopping = tf.keras.callbacks.EarlyStopping(
-        monitor="val_loss", patience=10, restore_best_weights=True)
+        monitor="val_loss", patience=5, restore_best_weights=True)
 
     checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_path, monitor='val_loss', verbose=1, save_weights_only=True,
         save_freq='epoch', mode='auto', save_best_only=True)
 
-    reduce_lr_on_plateau = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.5, patience=5)
+    reduce_lr_on_plateau = tf.keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.5, patience=2)
 
     callbacks_list = [early_stopping, checkpoint_callback, reduce_lr_on_plateau]
 
+    # Compiling the model
     optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
     model.compile(optimizer=optimizer,
                   loss={'centernet_loss': lambda y_true, y_pred: y_pred},
                   run_eagerly=True)
     model.summary()
 
-    print(f'The shape of the training data is: {len(image_names)}')
-
     model.fit(
-        x=train_gen,
+        x=ds_train,
         epochs=epochs,
-        validation_data=val_gen,
+        validation_data=ds_test,
         callbacks=callbacks_list,
     )
 
---
+++ training/prepare_dataset.py
@@ -0,0 +1,102 @@
+import cv2
+import math
+import numpy as np
+import random
+from data_preprocessing.gaussian import gaussian_radius, draw_gaussian
+from data_preprocessing.image_preprocessor import ImagePreprocessor
+
+
+class ObjectDetectionDataset:
+    def __init__(self, image_names, shuffle_data, batch_size, preprocessing_strategy, input_size, grayscale,
+                 downsample_factor, num_classes, max_objects):
+        self.__image_names = image_names
+        self.__shuffle_data = shuffle_data
+        self.__batch_size = batch_size
+        self.__input_size = input_size
+        self.__grayscale = grayscale
+        self.__image_preprocessor = ImagePreprocessor(preprocessing_strategy=preprocessing_strategy,
+                                                      target_shape=input_size,
+                                                      grayscale=grayscale)
+        self.__downsample_factor = downsample_factor
+        self.__output_size = input_size // downsample_factor
+        self.__num_classes = num_classes
+        self.__max_objects = max_objects
+        self.__ds_size = len(self.__image_names)
+
+    def create_datasets(self, train_ratio):
+        filelist_ds = tf.data.Dataset.from_tensor_slices(self.__image_names)
+
+        ds_train = filelist_ds.take(int(self.__ds_size * train_ratio))
+        ds_test = filelist_ds.skip(int(self.__ds_size * train_ratio))
+
+        ds_train = ds_train.map(self._combine_images_labels, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        ds_train = ds_train.shuffle(int(self.__ds_size * train_ratio))
+        ds_train = ds_train.batch(self.__batch_size)
+        ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
+
+        ds_test = ds_test.map(self._combine_images_labels, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        ds_test = ds_test.shuffle(int(self.__ds_size * (1 - train_ratio)))
+        ds_test = ds_test.batch(self.__batch_size)
+        ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
+
+        return ds_train, ds_test
+
+    def _tf_helper_func(self, file_path: tf.Tensor):
+        x = self._generate_X(file_path)
+        y = self._generate_y()
+        return x[0], x[1], x[2], x[3], x[4], x[5], y
+
+    def _combine_images_labels(self, x):
+        a, b, c, d, e, f, y = tf.py_function(self._tf_helper_func, [x],
+                                             Tout=[tf.float32, tf.float32, tf.float32,
+                                                   tf.float32, tf.float32, tf.float32, tf.float32])
+        return (a, b, c, d, e, f), y
+
+    def _generate_X(self, file):
+        channels = 1 if grayscale else 3
+        image = np.zeros((input_size, input_size, channels),
+                         dtype=np.float32)
+        hms = np.zeros((output_size, output_size, num_classes),
+                       dtype=np.float32)
+        whs = np.zeros((max_objects, 2), dtype=np.float32)
+        regs = np.zeros((max_objects, 2), dtype=np.float32)
+        reg_masks = np.zeros((max_objects), dtype=np.float32)
+        indices = np.zeros((max_objects), dtype=np.float32)
+
+        file = str(file.numpy())[2:-1]
+        img = cv2.imread(file)
+        scaled_image_dims = image_preprocessor.scaled_image_dims(img)
+        img = image_preprocessor.preprocess_image(img)
+        image = img
+
+        with open(f'{file[:-4]}.txt') as reader:
+            lines = reader.readlines()
+            bbox_index = 0
+            for line in lines:
+                object_class, x_center, y_center, w, h = line.split(' ')
+                object_class, x_center, y_center, w, h = \
+                    float(object_class), float(x_center), float(y_center), float(w), float(h)
+
+                x_center, y_center = x_center * scaled_image_dims[1], y_center * scaled_image_dims[0]
+                w, h = w * scaled_image_dims[1], h * scaled_image_dims[0]
+                x_center += (input_size - scaled_image_dims[1]) / 2
+                y_center += (input_size - scaled_image_dims[0]) / 2
+
+                x_center, y_center = x_center / downsample_factor, y_center / downsample_factor
+                w, h = w / downsample_factor, h / downsample_factor
+
+                ct = np.array([x_center, y_center], dtype=np.float32)
+                ct_int = ct.astype(np.int32)
+                radius = gaussian_radius((max(math.ceil(h), 1), max(math.ceil(w), 1)), min_overlap=0.3)
+                draw_gaussian(hms[:, :, int(object_class)], ct_int, int(radius), radius / 2)
+                whs[bbox_index] = 1. * w, 1. * h
+                regs[bbox_index] = ct - ct_int
+                reg_masks[bbox_index] = 1
+                indices[bbox_index] = (ct_int[1] * output_size + ct_int[0])
+                bbox_index += 1
+
+        return np.array(image), np.array(hms), np.array(whs), np.array(regs), \
+               np.array(reg_masks), np.array(indices)
+
+    def _generate_y(self):
+        return np.zeros(1)
